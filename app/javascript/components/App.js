a1b12ea884168c230bd1848244c20c045c3a7d85
jasonrfeliz <jason@meetmycampus.com>
2019-02-18 01:17:21 -0500

    initialize

---
+++ app/javascript/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Route, Link} from 'react-router-dom'
+import '../../assets/stylesheets/application.css'
+
+class App extends Component {
+  constructor(){
+    super()
+  }
+
+  render() {
+    const hello = (
+      <React.Fragment>
+        <h1>Hello World!</h1>
+        <p>Welcome to Rails-React Full Stack Template</p>
+      </React.Fragment>
+    )
+    return (
+      <React.Fragment>
+        {hello}
+      </React.Fragment>
+
+    )
+  }
+}
+
+export default App
