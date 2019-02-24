9fc8280fc2c6e25b3faa8dfdb75b5f3f990bb165
jasonrfeliz <jason@meetmycampus.com>
2019-02-23 20:17:53 -0500

    config/application.rb add api only option

--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,7 @@ module RailsReactTemplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-<<<<<<< HEAD
-    config.api_only = false
-=======
     config.api_only = true
->>>>>>> api-auth
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
