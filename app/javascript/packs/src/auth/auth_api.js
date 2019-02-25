7e6b15739a5be34b0695fc9551c625c60cc16132
jasonrfeliz <jason@meetmycampus.com>
2019-02-25 13:42:37 -0500

    added hearders to axio requests

--- app/javascript/packs/src/auth/auth_api.js
+++ app/javascript/packs/src/auth/auth_api.js
@@ -1,23 +1,30 @@
 import apiUrl from '../api_config.js'
 import axios from 'axios'
-
+import Cookies from 'js-cookie';
 
 export const signIn = (credentials) => {
   return axios.post(`${apiUrl}/sign_in`, credentials)
 }
 
 export const signUp = (credentials) => {
-  return axios.post(`${apiUrl}/sign_up`, credentials)
+  return axios.post(`${apiUrl}/sign_up`,credentials)
 }
 
 export const signOut = () => {
-  return axios.delete(`${apiUrl}/sign_out`)
+  return axios.delete(
+    `${apiUrl}/sign_out`,
+    {headers: { "X-CSRF-Token": Cookies.get("CSRF-TOKEN") } })
 }
 
 export const changePassword = (passwords) => {
-  return axios.patch(`${apiUrl}/change_pw`,passwords)
+  return axios.patch(
+    `${apiUrl}/change_pw`,
+    passwords,
+    { headers: { "X-CSRF-Token": Cookies.get("CSRF-TOKEN") } })
 }
 
 export const checkUser = () => {
-  return axios.post(`${apiUrl}/check_user`)
+  return axios.post(
+    `${apiUrl}/check_user`,
+    {  headers: { "X-CSRF-Token": Cookies.get("CSRF-TOKEN") } })
 }
